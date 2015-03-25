b2f966adec569d6ffc42f115e2b967af3e74bf88
Gotlight <gotlight.a@gmail.com>
2015-03-26 00:03:54 +0100

    Mockup button for reservation Mockup button for client zone

---
+++ Assets/TableReservation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TableReservation : MonoBehaviour
+{
+
+    public Text ScreenName;
+    void OnEnable()
+    {
+        GetComponentInChildren<Text>().text = DumbSingleton.Instance.organization.OrganizationName + " reservation";
+    }
+}
