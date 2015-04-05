eb0ea8c306d9546f71c95fed2c3d4754a85b57d7
Gotlight <gotlight.a@gmail.com>
2015-04-05 15:30:55 +0200

    Added external map link for android Fixed text size on Directions screen

---
+++ Assets/ExternalLinkLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExternalLinkLoader : MonoBehaviour {
+
+    public void RedirectToLink()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+            Application.OpenURL("https://www.google.cz/maps/search/50.0745494,14.437248919");
+    }
+}
--- Assets/Scripts/BackButtonHandler.cs
+++ Assets/Scripts/BackButtonHandler.cs
@@ -1,15 +1,7 @@
 ﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;
 
 public class BackButtonHandler : MonoBehaviour
 {
-    
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
 	void Update () {
 	    if (Input.GetKey(KeyCode.Escape)) 
 	    {
