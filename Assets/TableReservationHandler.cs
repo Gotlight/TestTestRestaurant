13786ce330da5261e707332543f17bc1dbd68f8c
Gotlight <gotlight.a@gmail.com>
2015-04-12 13:49:25 +0200

    Cosmetic changes - Menu list, menu category

---
+++ Assets/TableReservationHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Mime;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TableReservationHandler : MonoBehaviour
+{
+
+    public Image Screen;
+    private bool IsLoggedIn;
+    void OnEnable()
+    {
+        IsLoggedIn = Screen.GetComponent<TableReservation>().IsLoggedIn;
+    }
+
+    public void PlaceTableReservation()
+    {
+        if (IsLoggedIn)
+        {
+            
+        }
+        else
+        {
+            
+        }
+    }
+}
